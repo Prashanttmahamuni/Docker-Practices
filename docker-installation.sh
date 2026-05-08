5a5c8e926a5df57bfe95a5514e0057252fa9169c
Prashant Mahamuni <164340985+Prashanttmahamuni@users.noreply.github.com>
2026-05-08 20:04:11 +0530

    Update docker-installation.sh

--- docker-installation.sh
+++ docker-installation.sh
@@ -5,7 +5,7 @@ sudo yum update -y
                     
 # Install Docker
 sudo yum install docker -y
-                  
+                                
 # Start Docker service
 sudo systemctl start docker
 
